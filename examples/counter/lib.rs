b45b1cdd0bb7c2a6b1dc8d49aaccae7f27340e85
GiperMeon <gnatoffskiy43@gmail.com>
2023-10-11 22:39:32 +0700

    WE-8712: add example count

---
+++ examples/counter/lib.rs
@@ -0,0 +1,22 @@
+use we_contract_sdk::*;
+
+#[action]
+fn _constructor(int_value: Integer) {
+    set_storage!(integer => ("count", int_value));
+    set_storage!(binary => ("owner", get_tx_sender!()));
+}
+
+#[action]
+fn counter(step: Integer) {
+    let count: Integer = get_storage!(integer "count");
+    set_storage!(integer => ("count", count + step));
+}
+
+#[action]
+fn restore_counter(new_count: Integer) {
+    let owner: Binary = get_storage!(binary "owner");
+    require!(owner == get_tx_sender!());
+    set_storage!(integer => ("count", new_count));
+} 
+
+            
