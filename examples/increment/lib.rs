2c4bfbcb2cc2e5509a35e5eff22dda203df4edb6
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2023-10-31 16:43:14 +0700

    Added increment example

---
+++ examples/increment/lib.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+use we_contract_sdk::*;
+
+#[action]
+fn _constructor() {
+    set_storage!(integer => ("counter", 0));
+}
+
+#[action]
+fn increment_1() {
+    let counter = get_storage!(integer "counter");
+    set_storage!(integer => ("counter", counter + 1));
+}
+
+#[action]
+fn increment(step: Integer) {
+    let counter = get_storage!(integer "counter");
+    set_storage!(integer => ("counter", counter + step));
+}
