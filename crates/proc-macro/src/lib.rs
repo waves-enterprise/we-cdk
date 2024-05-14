53ac3aa1e198ed9106ff9d25cd00b8fad72c7a0b
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2024-05-14 20:46:16 +0700

    Added documentation

--- crates/proc-macro/src/lib.rs
+++ crates/proc-macro/src/lib.rs
@@ -2,6 +2,7 @@ mod generator;
 
 use proc_macro::TokenStream;
 
+/// Marks trait as an interface to another contract.
 #[proc_macro_attribute]
 pub fn interface(_attr: TokenStream, item: TokenStream) -> TokenStream {
     match generator::interface(item.into()) {
@@ -10,6 +11,7 @@ pub fn interface(_attr: TokenStream, item: TokenStream) -> TokenStream {
     }
 }
 
+/// Marks function as a called function.
 #[proc_macro_attribute]
 pub fn action(_attr: TokenStream, item: TokenStream) -> TokenStream {
     match generator::action(item.into()) {
